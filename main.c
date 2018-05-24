36f4178a464121393065a5f440289a5fc4a6e245
Haaris710 <31973970+Haaris710@users.noreply.github.com>
2018-05-24 13:04:42 -0400

    Tried to fix changes to bit shifting operations

--- main.c
+++ main.c
@@ -5,40 +5,43 @@
 //Exercise 1: LEDs
 void ledDisplay(uint8_t num) {
 	//USEFUL CONSTANTS
-	const uint32_t ALL_GPIO1 = 0x58000000;
-	const uint32_t ALL_GPIO2 = 0x0000007C;
+	const uint32_t ALL_LED_GPIO1 = 0x58000000;
+	const uint32_t ALL_LED_GPIO2 = 0x0000007C;
 	
 	//VARIABLES
-	uint32_t write = 0;
-	uint32_t extract = 0;
+	uint32_t buffer = 0;
 	
+	//Check for invalid input
 	if(num > 255 || num < 0)
 		return;
 	
-	//INITIALIZATION
+	//Initialization
 	LPC_GPIO1->FIODIR |= 0xB0000000;
 	LPC_GPIO2->FIODIR |= 0x0000007C;
 	
-	//CLEAR ALL
-	LPC_GPIO1->FIOCLR |= ALL_GPIO1;
-	LPC_GPIO2->FIOCLR |= ALL_GPIO2;
+	//Clear all LEDs
+	LPC_GPIO1->FIOCLR |= ALL_LED_GPIO1;
+	LPC_GPIO2->FIOCLR |= ALL_LED_GPIO2;
 	
-	//WRITE TO LED
-	LPC_GPIO1->FIOSET |= ((num & 0x80) >> 7) << 28);
-	LPC_GPIO1->FIOSET |= ((num & 0x40) >> 6) << 29);
-	LPC_GPIO1->FIOSET |= ((num & 0x20) >> 5) << 31);
-	//LPC_GPIO1->FIOSET = write;
+	//Build buffer for GPIO1
+	buffer |= (num & 0x01) << 28;
+	buffer |= ((num & 0x02) >> 1) << 29;
+	buffer |= ((num & 0x04) >> 2) << 31;
 	
-	//write = 0;
-	LPC_GPIO2->FIOSET |= ((num & 0x10) >> 4) << 6);
-	LPC_GPIO2->FIOSET |= ((num & 0x08) >> 3) << 5);
-	LPC_GPIO2->FIOSET |= ((num & 0x04) >> 2) << 4);
-	LPC_GPIO2->FIOSET |= ((num & 0x02) >> 1) << 2);
-	LPC_GPIO2->FIOSET |= ((num & 0x01) << 2);
-	//LPC_GPIO2->FIOSET = write;
+	//Write to register for GPIO1
+	LPC_GPIO1->FIOSET |= buffer;
+	buffer = 0;
+
+	//Build buffer for GPIO2
+	buffer |= ((num & 0x08) >> 3) << 2;
+	buffer |= ((num & 0x10) >> 4) << 3;
+	buffer |= ((num & 0x20) >> 5) << 4;
+	buffer |= ((num & 0x40) >> 6) << 5;
+	buffer |= ((num & 0x80) >> 7) << 6;
 	
-	
-	
+	//Write to register for GPIO1
+	LPC_GPIO2->FIOSET |= buffer;
+	buffer = 0;
 }
 
 int main(void) {
