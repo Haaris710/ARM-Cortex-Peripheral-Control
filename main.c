3cb28a22394010a01f14925f524b69ee8577fb64
Haaris710 <31973970+Haaris710@users.noreply.github.com>
2018-05-28 19:40:26 -0400

    Added manifest constants for bits
    Added manifest constants for bits to improve readability

--- main.c
+++ main.c
@@ -5,6 +5,25 @@
 #include <string.h>
 #include <RTL.h>
 
+#define BIT0 (0x1)
+#define BIT1 (0x1 << 1)
+#define BIT2 (0x1 << 2)
+#define BIT3 (0x1 << 3)
+#define BIT4 (0x1 << 4)
+#define BIT5 (0x1 << 5)
+#define BIT6 (0x1 << 6)
+#define BIT7 (0x1 << 7)
+#define BIT10 (0x1 << 10)
+#define BIT12 (0x1 << 12)
+#define BIT18 (0x1 << 18)
+#define BIT20 (0x1 << 20)
+#define BIT23 (0x1 << 23)
+#define BIT24 (0x1 << 24)
+#define BIT25 (0x1 << 25)
+#define BIT26 (0x1 << 26)
+#define BIT31 (0x1 << 31)
+
+
 //Exercise 1: LEDs
 void ledDisplay(uint8_t num) {
 	//NOTE: Bit shifiting right/left not combined to
@@ -30,20 +49,20 @@ void ledDisplay(uint8_t num) {
 	LPC_GPIO2->FIOCLR |= ALL_LED_GPIO2;
 	
 	//Build buffer for GPIO1
-	buffer |= ((num & 0x80) >> 7) << 28;
-	buffer |= ((num & 0x40) >> 6) << 29;
-	buffer |= ((num & 0x20) >> 5) << 31;
+	buffer |= ((num & BIT7) >> 7) << 28;
+	buffer |= ((num & BIT6) >> 6) << 29;
+	buffer |= ((num & BIT5) >> 5) << 31;
 	
 	//Write to register for GPIO1
 	LPC_GPIO1->FIOSET |= buffer;
 	buffer = 0;
 
 	//Build buffer for GPIO2
-	buffer |= ((num & 0x10) >> 4) << 2;
-	buffer |= (num & 0x08); //(Right shift three, left shift three)
-	buffer |= ((num & 0x04) >> 2) << 4;
-	buffer |= ((num & 0x02) >> 1) << 5;
-	buffer |= (num & 0x01) << 6; 
+	buffer |= ((num & BIT4) >> 4) << 2;
+	buffer |= (num & BIT3); //(Right shift three, left shift three)
+	buffer |= ((num & BIT2) >> 2) << 4;
+	buffer |= ((num & BIT1) >> 1) << 5;
+	buffer |= (num & BIT0) << 6; 
 	
 	//Write to register for GPIO1
 	LPC_GPIO2->FIOSET |= buffer;
@@ -58,23 +77,23 @@ void joyStickReadToPutty(void) {
 	//Read from GPIO1
 	buffer |= LPC_GPIO1->FIOPIN;
 	
-	if((buffer & 0x00800000) == 0) {
+	if((buffer & BIT23) == 0) {
 		printf("Up");
   }
-	else if((buffer & 0x01000000) == 0) {
+	else if((buffer & BIT24) == 0) {
 		printf("Right");
 	}
-	else if((buffer & 0x02000000) == 0) {
+	else if((buffer & BIT25) == 0) {
 		printf("Down");
 	}
-	else if((buffer & 0x04000000) == 0) {
+	else if((buffer & BIT26) == 0) {
 		printf("Left");
 	}
 	else
 		printf("NO DIR");
 	printf("\r");
 	
-	if((buffer & 0x00100000) == 0)
+	if((buffer & BIT20) == 0)
 		printf("Pressed");
 	else
 		printf("Not Pressed");
@@ -90,23 +109,23 @@ char* joyStickRead(void) {
 	//Read from GPIO1
 	buffer |= LPC_GPIO1->FIOPIN;
 	
-	if((buffer & 0x00800000) == 0) {
+	if((buffer & BIT23) == 0) {
 		strcat(output, "Up");
   }
-	else if((buffer & 0x01000000) == 0) {
+	else if((buffer & BIT24) == 0) {
 		printf("Right");
 	}
-	else if((buffer & 0x02000000) == 0) {
+	else if((buffer & BIT25) == 0) {
 		printf("Down");
 	}
-	else if((buffer & 0x04000000) == 0) {
+	else if((buffer & BIT26) == 0) {
 		printf("Left");
 	}
 	else
 		printf("NO DIR");
 	printf("\r");
 	
-	if((buffer & 0x00100000) == 0)
+	if((buffer & BIT20) == 0)
 		strcat(output,"\nPressed");
 	else
 		strcat(output,"\nNot Pressed");
@@ -121,7 +140,7 @@ void pushButtonRead(void) {
 	//Read from GPIO2
 	buffer |= LPC_GPIO2->FIOPIN;
 	
-	if((buffer & (0x01 << 10)) == 0)
+	if((buffer & BIT10) == 0)
 		printf("Push Button Pressed\r\n");
 }
 
@@ -131,21 +150,21 @@ void potentiometerRead(void) {
 	//INITIALIZATION
 	uint32_t buffer = 0;
 	
-	//clear bit 18,19
+	//clear bit 18,19 (provided code)
 	LPC_PINCON->PINSEL1 &= ~(0x03 << 18); 
 	//set bit 18
-	LPC_PINCON->PINSEL1 |= (0x01 << 18); 
+	LPC_PINCON->PINSEL1 |= BIT18; 
 	//set bit 12 to apply power
-	LPC_SC->PCONP |= (0x01 << 12);
-	//select AD0.2 pin, ADC clock 25MHz/5, enable ADC
+	LPC_SC->PCONP |= BIT12;
+	//select AD0.2 pin, ADC clock 25MHz/5, enable ADC (provided code)
 	LPC_ADC->ADCR = (1 << 2) | (4 << 8) | (1 << 21);
 	
 	
 	//Start conversion
-	LPC_ADC->ADCR |= (0x01 << 24);
+	LPC_ADC->ADCR |= BIT24;
 	
 	//Wait for conversion to complete
-	while((LPC_ADC->ADGDR & (0x01 << 31)) > 0) {
+	while((LPC_ADC->ADGDR & BIT31) > 0) {
 		printf("Waiting for conversion\r\n");
 	}
 	
